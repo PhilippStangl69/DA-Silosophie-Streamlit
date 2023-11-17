605dc2ebb5d0ae22be01de9a1b27b55ab848e852
Philipp Stangl <philipp.stangl@stud.it.htl-hl.ac.at>
2023-11-17 11:01:39 +0100

    Grafana

--- Stremlit-CSV.py
+++ Stremlit-CSV.py
@@ -5,10 +5,11 @@ import Filter
 import seaborn as sns
 import matplotlib.pyplot as plt
 import Plot
+import streamlit.components.v1 as components
 
 
 
-df = pd.read_csv("resources/wetterdaten2.csv")
+df = pd.read_csv("resources/filtered.csv")
 df = df.loc[:, ~df.columns.isin(['Datum', 'Zeit'])]
 lst_row = df.iloc[-1]
 stop_d_value=lst_row['Timestamp']
@@ -40,16 +41,24 @@ st.markdown('''
 ## Graph
 ''')
 
-filtered_notnull = filtered.dropna()
-option = st.selectbox(
-     'Select What do you want so see',
-    ('Value1','Temp1','Temp2','Temp3','Globalstrahlung','Gehaeusetemp','Windgeschw','Windrichtung','Lufttemperatur','rel Feuchte','Taupunkttemp','Luftdruck abs','Luftdruck red','Helligkeit Nord','Helligkeit Ost','Helligkeit Sued','Helligkeit West','Helligkeit','Niederschlag','NiedIntensitaet','NiedSumme','Niederschlagsart','Sonne Elevation','Sonne Azimut','Globalstrahlung'))
+#filtered_notnull = filtered.dropna()
+#option = st.selectbox(
+  #   'Select What do you want so see',
+ #   ('Value1','Temp1','Temp2','Temp3','Globalstrahlung','Gehaeusetemp','Windgeschw','Windrichtung','Lufttemperatur','rel Feuchte','Taupunkttemp','Luftdruck abs','Luftdruck red','Helligkeit Nord','Helligkeit Ost','Helligkeit Sued','Helligkeit West','Helligkeit','Niederschlag','NiedIntensitaet','NiedSumme','Niederschlagsart','Sonne Elevation','Sonne Azimut','Globalstrahlung'))
 
 
 
+#st.pyplot(Plot.get_plot(filtered_notnull))
+options = st.selectbox(
+    'Was wollen sie sehen',
+    ('Temperatur', 'Leistung')
+
+)
+if options == 'Leistung':
+    components.iframe(src="http://18.156.77.48:3000/d-solo/e55a8f8b-268d-487b-ba07-7e7507efddcc/timetest?orgId=1&panelId=1", height=400)
 
 
-st.pyplot(Plot.get_plot(filtered_notnull))
+
 #st.line_chart(filtered_notnull, x="Timestamp", y=option,height=500, width=1500)
 
 
